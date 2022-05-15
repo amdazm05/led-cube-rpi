408ed2ee221ec2ecfc2dfb01d81c067967738b32
Ahmed Azeem <amdali1998@yahoo.com>
2022-05-15 19:52:29 +0100

    Adding display feature and SH files to setup display

---
+++ SETUP.md
@@ -0,0 +1,2 @@
+#  Settings for display
+https://stackoverflow.com/questions/62643521/setting-custom-resolution-on-raspberry-pi-to-fit-11-display
--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
 import time
+from PIL import Image
+img = Image.open("image.jpg")
+img.show()
+ 
+#  
 
+import pygame 
+from time import sleep
+from pygame.locals import *
+pygame.init()
+WIDTH = 480
+HEIGHT = 480
+windowSurface = pygame.display.set_mode((WIDTH, HEIGHT), 0, 0)
+img = pygame.image.load("image.jpg")
+windowSurface.blit(img, (0, 0))
+pygame.display.flip()
 
+while(True):
+    sleep(1)
