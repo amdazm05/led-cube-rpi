cba07acadcbbf5dae8e3b640d504ca5524de0c8e
Ahmed Azeem <amdali1998@yahoo.com>
2022-05-20 18:54:12 +0100

    broken: code is not working

--- src/nft/nft.py
+++ src/nft/nft.py
@@ -9,22 +9,27 @@ class NFT:
    
     def __init__(self):
         pygame.init()
-        self.WIDTH = 480
-        self.HEIGHT = 480
+        self.WIDTH = 360
+        self.HEIGHT = 360
         self.image_path="image.jpg"
+        self.image_path1="image1.jpg"
         self.img =None
         self.offset=(180,240)
+        self.offset1=(240,240)
     
     def display(self):
         print(self.image_path,self.WIDTH,self.HEIGHT)
         windowSurface = pygame.display.set_mode((self.WIDTH, self.HEIGHT), 0, 0)
         self.img = pygame.image.load(self.image_path)
-        print(windowSurface.get_rect().center)
-        print(type(windowSurface.get_rect().center))
+        shadow = pygame.image.load(self.image_path1)
+        # print(windowSurface.get_rect().center)
+        # print(type(windowSurface.get_rect().center))
         
         logo_rect =self.img.get_rect(center=self.offset)
+        logo_rect1 =shadow.get_rect(center=self.offset1)
         # windowSurface.blit(self.img, (0, 0))
         windowSurface.fill((0,0,0))
         windowSurface.blit(self.img, logo_rect)
+        windowSurface.blit(shadow, logo_rect1)
         pygame.display.update()
 
