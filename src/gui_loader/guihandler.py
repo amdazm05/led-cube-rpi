35d23503ad7b2c65bc585709b259933481e11db7
Ahmed Azeem <amdali1998@yahoo.com>
2022-06-14 10:53:20 +0500

    feat: Added colorschemes and button colors

--- src/gui_loader/guihandler.py
+++ src/gui_loader/guihandler.py
@@ -2,35 +2,36 @@ import tkinter as tk
 import tkinter.messagebox as msgbox
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
+from tkinter import *
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.attributes( '-zoomed', True)
-        self.title("Hello Tkinter")
-        self.label_text = tk.StringVar()
-        self.label_text.set("My Name Is: ")
-        self.filename="Hello"
-        self.name_text = tk.StringVar()
+        # self.attributes( '-zoomed', True)
+        self.title("NFT - CUBE")
+        self.geometry('480x480')
+        self.configure(bg='yellow')
+        self.img = PhotoImage(file="background.png")
+        label = Label(
+            self,
+            image=self.img
+        )
+        label.place(x=0, y=0)
+        
+        hello_button = tk.Button(self, text="Load Image", command=self.LoadImage, font=('Arial Bold', 16))
 
-        self.label = tk.Label(self, textvar=self.label_text)
-        self.label.pack(fill=tk.BOTH, expand=1, padx=100, pady=10)
+        hello_button.place(x=320,y=410)
+        hello_button.config(bg="navy blue",fg="deeppink")
+        hello_button["border"]="0"
 
-        self.name_entry = tk.Entry(self, textvar=self.name_text)
-        self.name_entry.pack(fill=tk.BOTH, expand=1, padx=20, pady=20)
-
-        hello_button = tk.Button(self, text="Load Image", command=self.LoadImage)
-
-        hello_button.pack(side=tk.LEFT, padx=(20, 0), pady=(0, 20))
-
-        goodbye_button = tk.Button(self, text="Close Program", command=self.ExitProgram)
-        goodbye_button.pack(side=tk.RIGHT, padx=(0, 20), pady=(0, 20))
+        goodbye_button = tk.Button(self, text="Close Cube", command=self.ExitProgram,font=('Arial Bold', 16))
+        goodbye_button.place(x=40,y=410)
+        goodbye_button.config(bg="navy blue",fg="deeppink")
+        goodbye_button["border"]="0"
 
     def getfilename(self):
         return self.filename
-    def LoadImage(self):
-        message = "Hello there " + self.name_entry.get()
-        msgbox.showinfo("Hello", message)
+    def LoadImage(self): 
         Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
         self.filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
         print(self.filename +  " $")
